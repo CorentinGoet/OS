360f17dce6f0180cff28e042893f52052b909e42
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-12-01 23:28:45 +0100

    added ex7

--- TP1/ex7/ex7_waitpid.c
+++ TP1/ex7/ex7_waitpid.c
@@ -22,23 +22,38 @@ int main ()  {
             break;
         case 0 :
             // Le code retour du fils est 0. Il execute donc cette partie.
+            printf("Le premier fils a été créé.\n");
+            printf("Le premier fils s'endort.\n");
             sleep(5);
-	    printf("Je suis le premier fils\n");
-            printf("Le pid du premier fils est: %d\n", getpid());
+            printf("Le premier fils s'arrête. \n");
             exit(1);
             break;
         default:
             // Le père a un code retour positif il exécute cette partie.
+            code_retour = fork();   // crée le 2e fils
+            switch (code_retour)
+            {
+            case 0:
+                // Code exécuté par le 2e fils.
+                printf("Le second fils a été créé. \n");
+                printf("Le second fils s'endort. \n");
+                sleep(10);
+                printf("Le second fils s'arrête.\n");
+                exit(2);
+                break;
             
-            
-            wait(&code_erreur_fils);
-            if (WIFEXITED(code_erreur_fils) != 0){
-            	printf("le fils s'est terminé avec le statut : %d \n", WEXITSTATUS(code_erreur_fils));
-            }
-            if (WIFSIGNALED(code_erreur_fils) != 0){
-            	printf("Le signal ayant terminé le fils est : %d \n", WTERMSIG(code_erreur_fils));
+            default:
+                wait(&code_erreur_fils);
+                wait(&code_erreur_fils);
+                if (WIFEXITED(code_erreur_fils) != 0){
+                    printf("le fils s'est terminé avec le statut : %d \n", WEXITSTATUS(code_erreur_fils));
+                }
+                if (WIFSIGNALED(code_erreur_fils) != 0){
+                    printf("Le signal ayant terminé le fils est : %d \n", WTERMSIG(code_erreur_fils));
+                }
+                break;
+            }            
 	    }	
-    }
     // Cette partie est exécutée par les deux processus.
 
     return 0;
